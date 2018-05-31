5bdb0ff631d7735d7d21d7fbd4d95ee7b0b991b8
Ozones <39534933+Ozones@users.noreply.github.com>
2018-05-31 17:52:20 +0200

    Update Plot.py
    Made titles for axes and such, and made legend readable when all amino acids are used as dictionary

--- Plot.py
+++ Plot.py
@@ -8,9 +8,16 @@ import matplotlib.pyplot as plt
 
 from Testing import Total, AA_dic
 
+plt.figure().set_size_inches(18.5, 10.5)
+
 for i in range(0, len(AA_dic)):
      result = [item[AA_dic[i]] for item in Total]
      plt.plot(result)
 
-plt.legend(AA_dic)
-plt.savefig('Plot.png') #Save plot as figure instead of showing it
+#Makes everything in the plot pretty
+plt.title('Frequence of occurance for different amino acids at each position in protein sequences', fontsize = 20)
+plt.xlabel('Position in sequence', fontsize = 16)
+plt.ylabel('Number of occurances of an amino acid', fontsize = 16)
+plt.legend(AA_dic, bbox_to_anchor=(1.04,1), loc="upper left")
+
+plt.savefig('Plot.png', dpi = 300) #Save plot as figure instead of showing it
