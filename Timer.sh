df9648834c7401343e86b558c0003d730a8171eb
Ozones <39534933+Ozones@users.noreply.github.com>
2018-05-31 14:51:31 +0200

    Add files via upload
    Timer that averages 10 runs of Testing.py and outputs it into the terminal

---
+++ Timer.sh
@@ -0,0 +1,11 @@
+#Averages time for a script using the terminal
+
+
+#Installs the needed packages 
+sudo apt install linux-tools-common
+sudo apt install linux-tools-4.4.0-127-generic
+sudo apt install linux-cloud-tools-4.4.0-127-generic
+
+#Performs 10 rounds of the command stated after "-B"
+#Outputs to terminal
+perf stat -r 10 -B python3 Testing.py
